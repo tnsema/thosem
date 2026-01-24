00192451a58622a17ca4700e7ddda2d7b232f2eb
Thobile Sema <thobilesema@gmail.com>
2026-01-25 00:36:38 +0200

    making static

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export", // enables static export
+  images: {
+    unoptimized: true, // required for static export
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
+/*import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here *
+};
+
+export default nextConfig;*/
